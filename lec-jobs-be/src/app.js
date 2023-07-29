629bb9ff22fa06a71b238bd3a505dfd108b0e99d
sushantbramhacharya <sushantbramacharya@gmail.com>
2023-07-29 07:51:06 +0545

    BE db changes

--- lec-jobs-be/src/app.js
+++ lec-jobs-be/src/app.js
@@ -1,6 +1,80 @@
 const express = require("express");
 const fs=require("fs");
 const cors = require("cors");
+const mongoose=require("mongoose");
+
+//connect sting going for lec database
+const mongoDbURI="mongodb://localhost:27017/lec";
+//interview question port and protocol
+
+mongoose.connect(mongoDbURI,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+});
+
+const userSchema = new mongoose.Schema(
+    [
+        {
+          title: String,
+          description: String,
+          location: String,
+          job_type: String,
+          pay_rate_per_hr_dollar: Number,
+          skills: [String],
+          liked_by: [String],
+          viewed_by: [String],
+          id: Number,
+          user_id: Number,
+          post_by_username: String,
+          post_by_fullname: String,
+          post_date: String,
+          comments: []
+        }
+    ]
+);
+
+const User = mongoose.model("post",userSchema);
+
+User.createCollection().then((col)=>{
+    console.log("Collection",col,"created");
+}).catch((err)=>console.log(err));
+
+User.create(
+    [
+        {
+          title: "PHP Developer Required",
+          description: "For a client project PHP Developer is required",
+          location: "Kathmandu",
+          job_type: "Full Time",
+          pay_rate_per_hr_dollar: 10.0,
+          skills: [
+            "PHP",
+            "JS",
+            "HTML"
+          ],
+          liked_by: [
+            "test111",
+            "test1",
+            "test123"
+          ],
+          viewed_by: [
+            "test111",
+            "test1",
+            "test123"
+          ],
+          id: 2,
+          user_id: 1,
+          post_by_username: "test123",
+          post_by_fullname: "Test User",
+          post_date: "2023-06-10T09:24:07.659034",
+          comments: [
+           
+          ]
+        }
+    ]
+).then(()=>{
+    console.log("Created");
+})
 
 const app = express();
 
@@ -11,11 +85,16 @@ const PORT = 5000;
 app.get('/',(req,res)=>{
     res.status(200).send("This is response from BE");
 })
+
 app.get('/api/v1/posts',(req,res)=>{
     const posts = fs.readFileSync("./data/posts.json","utf-8").toString();
     res.status(200).send(posts);
 })
 
+app.get('/api/v1/user',(req,res)=>{
+    const posts = fs.readFileSync("./data/user.json","utf-8").toString();
+    res.status(200).send(posts);
+})
 
 app.listen(PORT,()=>{
 console.log("App is Running on http://localhost:"+PORT);
