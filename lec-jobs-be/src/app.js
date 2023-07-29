c451a767be3aff06971ebf33a3a88ea0606c66b0
sushantbramhacharya <sushantbramacharya@gmail.com>
2023-07-29 08:21:32 +0545

    user data from db

--- lec-jobs-be/src/app.js
+++ lec-jobs-be/src/app.js
@@ -12,69 +12,80 @@ mongoose.connect(mongoDbURI,{
     useUnifiedTopology:true
 });
 
-const userSchema = new mongoose.Schema(
-    [
-        {
-          title: String,
-          description: String,
-          location: String,
-          job_type: String,
-          pay_rate_per_hr_dollar: Number,
-          skills: [String],
-          liked_by: [String],
-          viewed_by: [String],
-          id: Number,
-          user_id: Number,
-          post_by_username: String,
-          post_by_fullname: String,
-          post_date: String,
-          comments: []
-        }
-    ]
+const userSchema = new mongoose.Schema({
+    // [
+    //     {
+    //       title: String,
+    //       description: String,
+    //       location: String,
+    //       job_type: String,
+    //       pay_rate_per_hr_dollar: Number,
+    //       skills: [String],
+    //       liked_by: [String],
+    //       viewed_by: [String],
+    //       id: Number,
+    //       user_id: Number,
+    //       post_by_username: String,
+    //       post_by_fullname: String,
+    //       post_date: String,
+    //       comments: []
+    //     }
+    // ]
+    email:String,
+    username:String,
+    fullname:String,
+    title:String,
+    skills:[String],
+    address:String,
+    job_type:String,
+    id:Number,
+    is_active:Boolean,
+    followers:[String],
+    followings:[String]}
 );
 
-const User = mongoose.model("post",userSchema);
+const User = mongoose.model("user",userSchema);
 
 User.createCollection().then((col)=>{
     console.log("Collection",col,"created");
 }).catch((err)=>console.log(err));
 
-User.create(
-    [
-        {
-          title: "PHP Developer Required",
-          description: "For a client project PHP Developer is required",
-          location: "Kathmandu",
-          job_type: "Full Time",
-          pay_rate_per_hr_dollar: 10.0,
-          skills: [
-            "PHP",
-            "JS",
-            "HTML"
-          ],
-          liked_by: [
-            "test111",
-            "test1",
-            "test123"
-          ],
-          viewed_by: [
-            "test111",
-            "test1",
-            "test123"
-          ],
-          id: 2,
-          user_id: 1,
-          post_by_username: "test123",
-          post_by_fullname: "Test User",
-          post_date: "2023-06-10T09:24:07.659034",
-          comments: [
+// User.create(
+//     [
+//         {
+//           title: "PHP Developer Required",
+//           description: "For a client project PHP Developer is required",
+//           location: "Kathmandu",
+//           job_type: "Full Time",
+//           pay_rate_per_hr_dollar: 10.0,
+//           skills: [
+//             "PHP",
+//             "JS",
+//             "HTML"
+//           ],
+//           liked_by: [
+//             "test111",
+//             "test1",
+//             "test123"
+//           ],
+//           viewed_by: [
+//             "test111",
+//             "test1",
+//             "test123"
+//           ],
+//           id: 2,
+//           user_id: 1,
+//           post_by_username: "test123",
+//           post_by_fullname: "Test User",
+//           post_date: "2023-06-10T09:24:07.659034",
+//           comments: [
            
-          ]
-        }
-    ]
-).then(()=>{
-    console.log("Created");
-})
+//           ]
+//         }
+//     ]
+// ).then(()=>{
+//     console.log("Created");
+// })
 
 const app = express();
 
@@ -91,10 +102,10 @@ app.get('/api/v1/posts',(req,res)=>{
     res.status(200).send(posts);
 })
 
-app.get('/api/v1/user',(req,res)=>{
-    User.find()
-    const posts = fs.readFileSync("./data/user.json","utf-8").toString();
-    res.status(200).send(posts);
+app.get('/api/v1/user',async(req,res)=>{
+    // const posts = fs.readFileSync("./data/user.json","utf-8").toString();
+    const user = await User.find({id:1});
+    res.status(200).send(user);
 })
 
 app.listen(PORT,()=>{
